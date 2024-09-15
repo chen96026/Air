874141de169c68a3dc4554fc5cecbb20528da1c7
dki2368 <tf661633icv@gmail.com>
2024-09-16 01:24:38 +0800

    0916 0124 論壇頁加排序選項 細項微調

--- Nora/js/forum_uploadImg.js
+++ Nora/js/forum_uploadImg.js
@@ -5,14 +5,14 @@ $(() => {
     
     // 更新已上傳圖片的預覽圖
     async function updatePreview() {
-        uploadImg.empty().hide();  // / 清除前一次更新產生的預覽圖, 防止調整 CSS 前的圖片顯示在畫面上
-
+        uploadImg.empty().hide();  // 清除前一次更新產生的預覽圖, 防止調整 CSS 前的圖片顯示在畫面上
+        
         // 透過 async / await 讓圖片依上傳順序讀取並產生預覽圖，避免實際顯示的排序混亂
         function readURL(file) {
             return new Promise((resolve, reject) => {
                 const imgReader = new FileReader();
                 
-                imgReader.onload = (e) => resolve(e.target.result); // 當圖片資訊讀取成功
+                imgReader.onload = (e) => resolve(e.target.result); // 當圖片資訊成功讀取完成時回傳
                 imgReader.onerror = (e) => reject(e);
                 
                 imgReader.readAsDataURL(file);
@@ -20,27 +20,27 @@ $(() => {
         }
         
         // 把預覽圖寫入 html
-        const moreImg = $(`<div>`, {class: 'forum_moreImg'});
-        
+        const moreImg = $(`<div>`, {class: 'forum_moreImg'});        
+        // 圖片區
         if(selectedFiles.length > 0) {
             
             for (let i = 0; i < selectedFiles.length; i++) {
                 const preview = $(`<div>`);
-                const closeIcon = $(`<span>`, {class: 'forum_deleteImg', 'data-index': i}); 
+                const closeIcon = $(`<span>`, {class: 'forum_deleteImg', 'data-index': i}); // 在 .forum_deleteImg 設置 CSS 讓游標滑過圖片時顯示 "X" 提示可取消上傳
                 const previewImg =  $(`<img>`);
                 
                 const file = selectedFiles[i]
                 try {
-                    const result = await readURL(file); 
+                    const result = await readURL(file); // selectedFiles[i] 的資料讀取完成才繼續往下執行
                     previewImg.attr('src', result)
                     
                     if (i === 0) {
-                        // 第一張預覽圖(左邊大區塊)
+                        // 加入第一張預覽圖(左邊大區塊)
                         preview.addClass('forum_cover');
                         previewImg.addClass('forum_coverImg')
                         uploadImg.append((preview.append(closeIcon).append(previewImg)));
                     } else {
-                        // 其他預覽圖(右邊小區塊)
+                        // 加入其他預覽圖(右邊小區塊)
                         preview.addClass('forum_preview');
                         previewImg.addClass('forum_previewImg');
                         moreImg.append((preview.append(closeIcon).append(previewImg)));
@@ -51,7 +51,7 @@ $(() => {
             }
             if (selectedFiles.length > 1) uploadImg.append(moreImg);
             
-        } else uploadImg.append($(`<div>`, {class: 'forum_noImg'}));
+        } else uploadImg.append($(`<div>`, {class: 'forum_noImg'}));    // 目前無上傳檔案
 
             
         // 更新文字顯示目前上傳圖片數量
@@ -70,7 +70,7 @@ $(() => {
             </div>
         `);
 
-        // 調整 CSS
+        // 調整 CSS 其他預覽圖的寬
         if (selectedFiles.length > 4) {
             $('.forum_preview').css('width', `${100 / (selectedFiles.length - 1)}%`);
         }
@@ -79,17 +79,17 @@ $(() => {
     }
 
 
-    // 選取上傳圖片用的 input 並讀取新上傳的圖片資料
-    uploadImg.on('change', '#forum_chooseImg_id', (e) => {
+    // 讀取新上傳的圖片資料
+    uploadImg.on('change', '#forum_chooseImg_id', function(e) {
         selectedFiles.push(...e.target.files);
-        e.target.value = "";
+        e.target.value = "";    // 清空 input 讓同張圖片能重複上傳
         
         updatePreview();
     });
 
 
-    // 刪除(取消上傳)點擊的圖片
-    uploadImg.on('click','.forum_deleteImg', function() {
+    // 刪除(取消上傳)被點擊的圖片
+    uploadImg.on('click', '.forum_deleteImg', function() {
         let index = $(this).data('index');
         selectedFiles.splice(index, 1);
 
