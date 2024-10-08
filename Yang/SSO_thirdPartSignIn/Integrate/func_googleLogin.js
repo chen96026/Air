6c4e6d606412eeac86a72c8427468ff85d28b7ac
kiwibird <blackgod00200@gmail.com>
2024-10-08 10:27:08 +0800

    1008 1026 imort路徑修正

--- Yang/SSO_thirdPartSignIn/Integrate/func_googleLogin.js
+++ Yang/SSO_thirdPartSignIn/Integrate/func_googleLogin.js
@@ -1,4 +1,4 @@
-import { auth } from "../initialAuth.js";
+import { auth } from "./initialAuth.js";
 import { signInWithPopup, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
 
 const btnGoogle = document.getElementById("googleLogin");
