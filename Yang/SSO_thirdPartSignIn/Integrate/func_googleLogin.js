005de3d2d574ab14bdaf1a5727b187c058dafccc
kiwibird <blackgod00200@gmail.com>
2024-10-08 16:04:37 +0800

    1008 1604 第三方登入，icon和判斷是否新帳號

--- Yang/SSO_thirdPartSignIn/Integrate/func_googleLogin.js
+++ Yang/SSO_thirdPartSignIn/Integrate/func_googleLogin.js
@@ -2,28 +2,79 @@ import { auth } from "./initialAuth.js";
 import { signInWithPopup, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
 
 const btnGoogle = document.getElementById("googleLogin");
+let iconBlob;
 // Google登入
 btnGoogle.addEventListener("click", (e) => {
     const provider = new GoogleAuthProvider();
     provider.setCustomParameters({
-        prompt: "consent", // 強制要求用戶每次都需要授權
+        prompt: "select_account" // 讓用戶可以選擇帳號
     });
     signInWithPopup(auth, provider)
         .then((result) => {
-            // This gives you a Google Access Token. You can use it to access the Google API.
-            const credential = GoogleAuthProvider.credentialFromResult(result);
-            const token = credential.accessToken;
-            console.log(credential);
-            
-            // The signed-in user info.
             const user = result.user;
             console.log(user);
-            // IdP data available using getAdditionalUserInfo(result)
-            // ...
-            window.location.href = "http://127.0.0.1:5500/Integrate/main.html";
+
+            // 上次登入時間 = 創建時間，為新帳號，需要把資料送進資料庫
+            if (user.metadata.creationTime == user.metadata.lastSignInTime) {
+                urlToBlob(iconUrl).then(blob => { 
+                    iconBlob = blob;
+                    let data = {
+                        email: user.email,
+                        password: "1111111",
+                        name: user.displayName,
+                        icon: iconBlob
+                    };
+                    
+                    fetch('網址', {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify(data)
+                    })
+                        .then(response => {
+                            if (!response.ok) {
+                                throw new Error('Error:login()');
+                            }
+                            return response.json();
+                        })
+                        .then(data => {
+                            console.log('Success:', data);
+                            window.location.href = "http://127.0.0.1:5500/Integrate/main.html";
+                        })
+                        .catch(error => {
+                            console.error('err:', error);
+                        });
+                    
+                })
+
+                // 上次登入時間 != 創建時間，為舊帳號，不需送進資料庫
+            } else {
+                window.location.href = "";
+            }
+
         }).catch((error) => {
             const errorCode = error.code;
             const errorMessage = error.message;
             console.log("google問題\n" + errorCode + ": " + errorMessage);
         });
 });
+
+// 圖片url轉成BLOB
+async function urlToBlob(url) {
+    try {
+        // 使用 fetch 來取得圖片資料
+        const response = await fetch(url);
+
+        // 檢查回應狀態是否正常
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        // 將回應的資料轉為 Blob
+        const blob = await response.blob();
+        return blob; // 這裡返回 Blob 物件
+    } catch (error) {
+        console.error('Error fetching and converting to Blob:', error);
+    }
+}
--- Yang/SSO_thirdPartSignIn/Integrate/func_lineLogin.js
+++ Yang/SSO_thirdPartSignIn/Integrate/func_lineLogin.js
@@ -7,22 +7,69 @@ const provider = new OAuthProvider('oidc.line_login');
 btnLine.addEventListener("click", (e) => {
     signInWithPopup(auth, provider)
         .then((result) => {
-            // User is signed in.
-            // IdP data available using getAdditionalUserInfo(result)
-
-            // Get the OAuth access token and ID Token
-            console.log(result);
-            const credential = OAuthProvider.credentialFromResult(result);
-            const token = credential.accessToken;
-            console.log(credential);
-
             const user = result.user;
             console.log(user);
 
-            window.location.href = "http://127.0.0.1:5500/Integrate/main.html";
+            // 上次登入時間 = 創建時間，為新帳號，需要把資料送進資料庫
+            if (user.metadata.creationTime == user.metadata.lastSignInTime) {
+                urlToBlob(iconUrl).then(blob => {
+                    iconBlob = blob;
+                    let data = {
+                        email: "lineEmail",
+                        password: "2222222",
+                        name: user.displayName,
+                        icon: iconBlob
+                    };
+
+                    fetch('網址', {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify(data)
+                    })
+                        .then(response => {
+                            if (!response.ok) {
+                                throw new Error('Error:login()');
+                            }
+                            return response.json();
+                        })
+                        .then(data => {
+                            console.log('Success:', data);
+                            window.location.href = "http://127.0.0.1:5500/Integrate/main.html";
+                        })
+                        .catch(error => {
+                            console.error('err:', error);
+                        });
+
+                })
+
+                // 上次登入時間 != 創建時間，為舊帳號，不需送進資料庫
+            } else {
+                window.location.href = "";
+            }
         }).catch((error) => {
             const errorCode = error.code;
             const errorMessage = error.message;
             console.log("line問題\n" + errorCode + ": " + errorMessage);
         });
 });
+
+// 圖片url轉成BLOB
+async function urlToBlob(url) {
+    try {
+        // 使用 fetch 來取得圖片資料
+        const response = await fetch(url);
+
+        // 檢查回應狀態是否正常
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        // 將回應的資料轉為 Blob
+        const blob = await response.blob();
+        return blob; // 這裡返回 Blob 物件
+    } catch (error) {
+        console.error('Error fetching and converting to Blob:', error);
+    }
+}
