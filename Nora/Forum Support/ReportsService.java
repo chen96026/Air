c5eec2c0c68d40f20a68de4fdcfe13ef3a0f86f7
dki2368 <tf661633icv@gmail.com>
2024-10-19 23:39:45 +0800

    1019 2339 後台js 論壇管理部分修改

--- Nora/Forum Support/ReportsService.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.Service;
-
-import com.example.demo.Model.Reports;
-
-public interface ReportsService {
-	public Reports addReport (Long userId, Long postId);
-	public void deleteAllReports (Long postId);
-}
